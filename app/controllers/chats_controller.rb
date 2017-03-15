973af7bd196d5dfb9a069ef7f1777fd19c13973a
Gurminder Bhachu <gurminderbhachu@hotmail.com>
2017-03-15 15:17:14 +0000

    added views for chat, update controller functions

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -4,6 +4,26 @@ class ChatsController < ApplicationController
     @chat = Chat.all
   end
 
-  
+  def new
+    @chat = Chat.new
+  end
+
+  def create
+    @chat = current_user.chat.build(chat_params)
+    if @chat.save
+      flash[:success] = 'Chat room added!'
+      redirect_to chat_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def chat_params
+    params.require(:chat).permit(:chat_recipient, :chat_initiator)
+  end
+
+
 
 end
