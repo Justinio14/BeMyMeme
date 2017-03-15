a01cc9bbe87193e21bacc4543ad612e0bab8b611
Matthew Perry <thesedatedprince@gmail.com>
2017-03-15 17:58:12 +0000

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -55,10 +55,6 @@ ActiveRecord::Schema.define(version: 20170315141545) do
     t.index ["chat_id"], name: "index_messages_on_chat_id", using: :btree
   end
 
-  create_table "student", id: :integer, force: :cascade do |t|
-    t.text "name", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
