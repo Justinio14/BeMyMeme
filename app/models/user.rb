d41e607590f0b22491b9b37282e2f3f3af93d9f4
olwend <olwend@gmail.com>
2017-03-16 07:08:52 +0000

    shoulda paperclip tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   validates :username, length: { minimum: 3 }, uniqueness: true, presence: true
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  validates_attachment :image,
+  content_type: {content_type: /\Aimage\/.*\Z/},
+  size: {in: 0..1.megabytes }
   has_many :chats
   has_many :memes
   has_many :blocks
