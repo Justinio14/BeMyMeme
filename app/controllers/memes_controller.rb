cf717f0ba0e0f6f6546adf67325008f20e0a141a
Matthew Perry <thesedatedprince@gmail.com>
2017-03-16 16:57:17 +0000

    started modal - render issues

--- app/controllers/memes_controller.rb
+++ app/controllers/memes_controller.rb
@@ -3,5 +3,11 @@ class MemesController < ApplicationController
   def index
     @memes = Meme.all
   end
-  
+
+  def meme_gallery
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
 end
