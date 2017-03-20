3ef9bf03ed58f6adf1db3ac4841321fef5ebdf76
herecomesjaycee <jaycee.cheong04@gmail.com>
2017-03-20 11:35:11 +0000

    Implement action cable for real-time chat

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -7,8 +7,16 @@ class ChatChannel < ApplicationCable::Channel
     stop_all_streams
   end
 
-  def speak
+  def speak(data)
     message_params = data['message'].each_with_object({}) do |el, hash|
       hash[el.values.first] = el.values.last
+    end
+
+    Message.create(message_params)
+    
+    ActionCable.server.broadcast(
+      "chats-#{current_user.id}",
+      message: message_params
+    )
   end
 end
