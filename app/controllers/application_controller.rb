b814c2fbdead897cba228eb58af61c709a1f0466
herecomesjaycee <jaycee.cheong04@gmail.com>
2017-03-23 10:54:16 +0000

    Homepage redesign - still need gif

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,9 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
       profiles_path
     end
+
+  def after_sign_up_path_for(resource)
+      profile_path(current_user)
+  end
   # protect_from_forgery with: :exception
 end
